47318f2fbe1da5681fbb92ce5ce9e5b802a65f4f
Kyle Gibson <kyle.gibson2@snhu.edu>
2024-07-30 13:40:50 -0600

    HelloWorld Content

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/*
+* Kyle Gibson
+* Testing new IDE 
+* Simple HelloWorld with git
+*/
+
+#include <iostream>
+
+int main (){
+    std::cout << "Hello World!" << std::endl;
+    
+    return 0;
+}
